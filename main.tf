079c6de8febb356e19851691c4495a7476fe8c6b
Syedsuhaan <113284613+Syedsuhaan@users.noreply.github.com>
2023-01-02 16:57:47 +0530

    Add files via upload
    added address space through terraaform

--- main.tf
+++ main.tf
@@ -19,3 +19,12 @@ resource "azurerm_storage_account" "my_storageacc" {
     env = "prod"
   }
 }
+
+resource "azurerm_virtual_network" "syed_vnet" {
+  name                = "vnet_syed"
+  location            = "eastus"
+  resource_group_name = "syedresg"
+  address_space       = ["10.0.0.0/16"]
+ 
+
+}
